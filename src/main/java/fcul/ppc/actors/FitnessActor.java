25212b05392a8b6790523ac33480f9c070ac7f1c
brunocotrim1 <bruno.cotrim1@gmail.com>
2023-11-13 00:55:22 +0000

    Initial version

--- src/main/java/fcul/ppc/actors/FitnessActor.java
+++ src/main/java/fcul/ppc/actors/FitnessActor.java
@@ -4,6 +4,10 @@ import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
 import akka.actor.Props;
 import fcul.ppc.message.PopulationMessage;
+import fcul.ppc.parallelization.ParallelFW;
+import fcul.ppc.utils.Individual;
+
+import static fcul.ppc.utils.Utils.POP_SIZE;
 
 public class FitnessActor extends AbstractActor {
     private ActorRef bestIndividualActor;
@@ -12,15 +16,20 @@ public class FitnessActor extends AbstractActor {
         this.bestIndividualActor = bestIndividualActor;
     }
 
+    private void ParallelMeasureFitness(Individual[] population) {
+        ParallelFW.doInParallel(((startIndex, endIndex) -> {
+            for (int i = startIndex; i < endIndex; i++) {
+                population[i].measureFitness();
+            }
+        }), POP_SIZE);
+    }
 
     @Override
     public Receive createReceive() {
         return receiveBuilder()
                 .match(PopulationMessage.class, message -> {
 
-                    for (int i = 0; i < message.getPopulation().length; i++) {
-                        message.getPopulation()[i].measureFitness();
-                    }
+                    ParallelMeasureFitness(message.getPopulation());
                     bestIndividualActor.tell(message, getSelf());
 
                 })
